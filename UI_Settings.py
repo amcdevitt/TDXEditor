66e73d268ab8e931ceed9c9c66afccc9ac95e7ef
Aaron McDevitt <amcdevitt@gmail.com>
2020-11-21 19:35:53 -0500

    Initial commit

---
+++ UI_Settings.py
@@ -0,0 +1,27 @@
+class UI_Settings():
+    def __init__(self):
+        self.ColorSave     = 'dark green'
+        self.ColorCommit   = 'lawn green'
+                           
+        self.ColorLoad     = 'RoyalBlue1'
+        self.ColorTest     = 'light sky blue'
+                           
+        self.ColorExit     = 'red'
+        self.ColorClear    = 'light coral'
+                           
+        self.ColorEdit     = 'gold'           
+        self.ColorCopy     = 'DarkGoldenrod2'
+        self.ColorText     = 'lemon chiffon'
+                           
+        self.ColorBGdkGrey = 'grey55'
+        self.ColorBGltGrey = 'grey75'
+        self.ColorBack1    = 'grey85'
+        self.ColorBGnoGrey = 'grey95'
+
+        self.ColorPassive  = 'light grey'
+        self.ColorActive   = 'light blue'
+        self.ColorDisabled = 'grey'
+
+
+        self.ColorInput    = 'white'
+        self.ColorInputFL  = self.ColorInput
